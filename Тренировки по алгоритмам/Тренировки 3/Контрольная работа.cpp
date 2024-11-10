64ee154319348c769e9980fb40d9091f07731274
Nazar <nvzakrevskii@edu.hse.ru>
2024-11-10 23:08:29 +0300

    new tasks done

--- Тренировки по алгоритмам/Тренировки 3/Контрольная работа.cpp
+++ Тренировки по алгоритмам/Тренировки 3/Контрольная работа.cpp
@@ -1,46 +1,36 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <unordered_map>
-#include <climits>
 
 int main() {
     int n, r, place, k;
     std::cin >> n >> k >> r >> place;
-    std::unordered_map<int, std::pair<int, int>> mp;
-    int current = 1;
-    for (int i = 1; i <= n; i++) {
-        mp[i] = {current % k, i / 2 + i % 2};
-        current++;
-    }
-
-    int need = mp[2 * (r - 1) + place % 2].first;
-    int peters_r = r;
-    int pre_ans = INT_MAX;
-    int number = -1;
-    int ryad = -1;
-    for (auto el : mp) {
-        if (el.first != (2 * (r - 1) + place % 2) && el.second.first == need) {
-            if (std::abs(peters_r - el.second.second) <= pre_ans) {
-                if (std::abs(peters_r - el.second.second) == pre_ans) {
-                    pre_ans = std::abs(peters_r - el.second.second);
-                    if (ryad < el.second.second) {
-                        ryad = el.second.second;
-                        number = el.first;
-                    }
-
-                } else {
-                    pre_ans = std::abs(peters_r - el.second.second);
-                    ryad = el.second.second;
-                    number = el.first;
-                }
-            }
+    int peters_number = (r - 1) * 2 + (place % 2 == 0 ? 2 : 1);
+    int nearest = 1e9;
+    int answer_row = -1;
+    int answer_place = -1;
+    if (peters_number - k > 0) {
+        int el = peters_number - k;
+        int current_row = (el + 1) / 2;
+        if (abs(r - current_row) <= nearest) {
+            nearest = abs(r - current_row);
+            answer_row = current_row;
+            answer_place = (el % 2 == 0) ? 2 : 1;
         }
     }
-    if (pre_ans == INT_MAX) {
+    if (peters_number + k <= n) {
+        int el = peters_number + k;
+        int current_row = (el + 1) / 2;
+        if (abs(r - current_row) <= nearest) {
+            nearest = abs(r - current_row);
+            answer_row = current_row;
+            answer_place = (el % 2 == 0) ? 2 : 1;
+        }
+    }
+    if (nearest == 1e9) {
         std::cout << -1;
         return 0;
     }
-    std::cout << ryad + 1 << ' ' << (number % 2 == 0 ? 2 : 1);
+    std::cout << answer_row << ' ' << answer_place;
     return 0;
 }
